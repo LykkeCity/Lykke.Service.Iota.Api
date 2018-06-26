0e809ef025fd89dfe4a20bbe5b7c64c6feb39933
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-06-26 13:11:49 +0300

    Add [GET] /api/addresses/{address}/balance

--- src/Lykke.Service.Iota.Api/Controllers/AddressesController.cs
+++ src/Lykke.Service.Iota.Api/Controllers/AddressesController.cs
@@ -1,10 +1,15 @@
 ﻿using Lykke.Service.BlockchainApi.Contract.Addresses;
+using Lykke.Service.Iota.Api.Core.Domain;
+using Lykke.Service.Iota.Api.Core.Repositories;
 using Lykke.Service.Iota.Api.Core.Services;
+using Lykke.Service.Iota.Api.Core.Shared;
 using Lykke.Service.Iota.Api.Settings;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Tangle.Net.Utils;
 
 namespace Lykke.Service.Iota.Api.Controllers
 {
@@ -12,12 +17,18 @@ namespace Lykke.Service.Iota.Api.Controllers
     public class AddressesController : Controller
     {
         private readonly IIotaService _iotaService;
+        private readonly INodeClient _nodeClient;
+        private readonly IBalancePositiveRepository _balancePositiveRepository;
         private readonly IotaApiSettings _settings;
 
         public AddressesController(IIotaService iotaService,
+            INodeClient nodeClient,
+            IBalancePositiveRepository balancePositiveRepository,
             IotaApiSettings settings)
         {
             _iotaService = iotaService;
+            _nodeClient = nodeClient;
+            _balancePositiveRepository = balancePositiveRepository;
             _settings = settings;
         }
 
@@ -31,8 +42,42 @@ namespace Lykke.Service.Iota.Api.Controllers
             });
         }
 
+        [HttpGet("{address}/balance")]
+        public async Task<IActionResult> GetAddressBalance([Required] string address)
+        {
+            var isVirtualAddress = address.StartsWith(Consts.VirtualAddressPrefix);
+            if (isVirtualAddress)
+            {
+                var isAddressCompromised = false;
+                var inputs = await _iotaService.GetVirtualAddressInputs(address);
+                foreach (var input in inputs.Where(f => f.Balance > 0))
+                {
+                    if (await _nodeClient.HasCashOutTransaction(input.Address))
+                    {
+                        isAddressCompromised = true;
+                        break;
+                    }
+                }
+
+                return Ok(new
+                {
+                    assetId = Asset.Miota.Id,
+                    balance = await _iotaService.GetVirtualAddressBalance(address),
+                    block = Timestamp.UnixSecondsTimestamp,
+                    isAddressCompromised
+                });
+            }
+
+            return Ok(new
+            {
+                assetId = Asset.Miota.Id,
+                balance = await _nodeClient.GetAddressBalance(address, _settings.MinConfirmations),
+                block = Timestamp.UnixSecondsTimestamp,
+                isAddressCompromised = await _nodeClient.HasCashOutTransaction(address)
+            });
+        }
+
         [HttpGet("{address}/explorer-url")]
-        [ProducesResponseType(typeof(AddressValidationResponse), (int)HttpStatusCode.OK)]
         public string[] GetAddressExplorerUrl([Required] string address)
         {
             return new string[]
@@ -42,7 +87,6 @@ namespace Lykke.Service.Iota.Api.Controllers
         }
 
         [HttpGet("{address}/underlying")]
-        [ProducesResponseType(typeof(AddressValidationResponse), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetAddressUnderlying([Required] string address)
         {
             var underlyingAddress = await _iotaService.GetRealAddress(address);
