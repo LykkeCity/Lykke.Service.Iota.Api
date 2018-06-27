100d91faf8ccabff410bc61ee48d75a4b64b681e
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-06-27 12:36:07 +0300

    Fix inputs ordering

--- src/Lykke.Service.Iota.Api.AzureRepositories/AddressInput/AddressInputRepository.cs
+++ src/Lykke.Service.Iota.Api.AzureRepositories/AddressInput/AddressInputRepository.cs
@@ -7,6 +7,7 @@ using Lykke.SettingsReader;
 using Lykke.Service.Iota.Api.Core.Domain.Address;
 using Lykke.Service.Iota.Api.Core.Repositories;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lykke.Service.Iota.Api.AzureRepositories
 {
@@ -23,7 +24,8 @@ namespace Lykke.Service.Iota.Api.AzureRepositories
 
         public async Task<IEnumerable<IAddressInput>> GetAsync(string addressVirtual)
         {
-            return await _table.GetDataAsync(GetPartitionKey(addressVirtual));
+            return (await _table.GetDataAsync(GetPartitionKey(addressVirtual)))
+                .OrderBy(f => f.Index);
         }
 
         public async Task<IAddressInput> GetAsync(string addressVirtual, string address)
